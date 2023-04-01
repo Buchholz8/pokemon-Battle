d6409a2cb83226d3d4ffc2a2525f7864a019d1d8
markb <markbuchholz8@gmail.com>
2023-04-01 15:43:31 -0600

    html fixes

--- js/Battle.js
+++ js/Battle.js
@@ -1,23 +1,49 @@
+
 let player_pick_json = Cookies.get(`player_select`);
-let ai_pick_json = Cookies.get(`cumputer`);
+let ai_pick_json = Cookies.get(`Computer`);
 
 let arena = document.getElementById(`fight_container`)
+if(ai_pick_json === undefined){
+    arena.insertAdjacentHTML(`afterbegin` , `<p> Error in picking </p>`)
+}else {
+    let ai_pick = JSON.parse(ai_pick_json)
+   arena.insertAdjacentHTML(`afterbegin` , `
+   <img src=${ai_pick[`Sprite`]}>
+   <div id="ai_battle">
+        <p id="ai_hp">${ai_pick[`Hp`]}</p>
+        <h4>${ai_pick[`Name`]}</h4>
+        
+    </div>
+   `) 
+}
 if(player_pick_json === undefined){
-arena.insertAdjacentHTML(`afterend` , `<p> Error in picking </p>`)
+arena.insertAdjacentHTML(`afterbegin` , `<p> Error in picking </p>`)
 }else{
     let player_pick = JSON.parse(player_pick_json);
-let ai_pick = JSON.parse(ai_pick_json);
     arena.insertAdjacentHTML(`afterbegin` , (  `
-    <div>
-        <img ${pokemon[`Sprite`]}
-        <h4>${pokemon[`Name`]}</h4>
-        <p>${pokemon[`Hp`]}</p>
-    </div>
-    <div>
-    <img ${pokemon[`Sprite`]}
-        <h4>${pokemon[`Name`]}</h4>
-        <p>${pokemon[`Hp`]}</p>
-        <button>${pokemon[`Moves`]}</button>
+    <img src=${player_pick[`Sprite`]}>
+    <div id="player_battle">
+        <h4 id="player_hp">${player_pick[`Hp`]}</h4>
+        <p>${player_pick[`Name`]}</p>
+        <div id="moves">
+        <button id="move_button">${player_pick[`Moves`][0][`Move_name`]}</button>
+        <button id="move_button">${player_pick[`Moves`][1][`Move_name`]}</button>
+        <button id="move_button">${player_pick[`Moves`][2][`Move_name`]}</button>
+        <button id="move_button">${player_pick[`Moves`][3][`Move_name`]}</button>
+        </div>
+        
     </div>
 `))
 }
+
+let ai_hp = document.getElementById(`ai_hp`)
+let player_hp = document.getElementById(`player_hp`)
+function winner(details){
+if(player_hp <= 0){
+    player_hp.innerHTML[`target`] = `<h4 id="player_hp"> Game Over </h4>`
+} if(ai_hp <= 0){
+    player_hp.innerHTML[`target`] = `<h4 id="player_hp"> Winner </h4>`
+}
+}
+
+    
--- js/characters.js
+++ js/characters.js
@@ -2,7 +2,7 @@ let pokemon = [
     {
         Name:`Poliwrath`,
         Sprite:"/images/062.png",
-        Hp:150,
+        Hp:0,
         Moves: [
             {
                Move_name:`Split Spiral Punch`,
@@ -69,24 +69,3 @@ let pokemon = [
             ]
     },
 ]
-function Choices(details){
-    let player1=undefined;
-    for(let i=0 ; i <pokemon.length; i++){
-        if(pokemon[i][`Name`] === details[`target`].getAttribute(`Key_Name`) ){
-            player1 = pokemon[i]
-        }
-    }
-    let ai = pokemon[0]
-    let player_poke_json = JSON.stringify(player1)
-    let ai_player_jsno = JSON.stringify(ai)
-    Cookies.set(`player_health` , player1);
-    Cookies.set(`ai_health` , ai);
-    Cookies.set(`player_select` , player_poke_json);
-    Cookies.set(`Computer` , ai_player_jsno);
-    location.href= `pages/Battle.html`
-    }
-
-let pick_button = document.querySelectorAll(`.play_as_button`)
-for(let i=0; i < pick_button.length ; i++){
-    pick_button[i].addEventListener(`click` , Choices)
-}
