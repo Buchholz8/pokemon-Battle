04cb35f993858099b9a1b8b2498febd2a7ea8a0b
markb <markbuchholz8@gmail.com>
2023-04-01 11:23:05 -0600

    changes to move sets

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let pokemon = [
         Moves: [
             {
                Move_name:`Split Spiral Punch`,
-               Move_Power:70, 
+               Move_power:70, 
             },
             {
                 Move_name:`Wake-up Slap`,
@@ -69,38 +69,15 @@ let pokemon = [
             ]
     },
 ]
-function spawn(details){
-    let p = pokemon_list[Math.floor(math.random()*pokemon_list.length)];
-    let pokemon_list = new pkm(p[0], p[1], p[2], p[3]);
-    if(details){
-        for (i=0 ; i<pkm ; i++){
-            document.getElementById(`move_` + i).value = pokemon[`moves`];
-        }
-    }
-    return pkm;
-}
-let player_1 = spawn(true)
-player_1 = document.getElementById(`plyer_img`)
-player_1.src = pokemon.Sprite
-
-let player_2 = spawn(false)
-player_2 = document.getElementById(`ai_img`)
-player_2.src = pokemon.Sprite
-
-for(let i =0 ; i<pkm; i++){
-    let button = document.querySelectorAll(`.button`);
-    let moves = player_1.Moves[i];
-    function sequence(button, Moves, player_1, player_2){
-        button.addEventListener(`click` , function(e){
-            attack(move, player_1, player_2, `ai_health`, ``)
-            attack, player_2.moves[Math.floor(Math.random()*3)], player_2,player_1, 'Player_health', `Foe`;
-            Cookies.set(`Battle`)
-        });
-    }
-    addHandler(button,moves, player_1, player_2)
-}
-function attack(move, ai, reciever, hp, player){
-    let power = 
-   reciever.Hp -=Math.floor(power);
-   document.getElementById(``)
+let Moves_n = pokemon.Moves.Move_name
+for(let i = 0; i < pokemon.length ; i++){
+    document.body.insertAdjacentHTML(`beforeend` , `<div class="prod_div>
+    <img src="${pokemon[i][`Sprite`]}</img>
+    <h1>${pokemon[i][`Hp`]}</h1>
+    <h2>${pokemon[i][`Name`]}</h2>
+    <p>${pokemon[i][`Moves_n`]}</p>
+    <p>${pokemon[i].Moves}</p>
+    <button class="Play_button" id="${pokemon[i][`Name`]}"> Play As</button>
+    </div>
+    `);
 }
